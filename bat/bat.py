34564a19086782550eb2bf22561839469bcae0fc
omarsakrlaravel <124620419+omarsakrlaravel@users.noreply.github.com>
2024-12-13 15:33:36 +0300

    path

--- bat/bat.py
+++ bat/bat.py
@@ -1,9 +1,11 @@
 import os
 import argparse
 
-PYTHON_DIR = "d:/Python/bin"
-BAT_DIR = "d:/Python/bin/bat"
-ALIAS_DIR = "d:/Python/bin/alias"
+current_file_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_file_path)
+PYTHON_DIR = os.path.join(current_dir, "../")
+BAT_DIR = current_dir
+ALIAS_DIR = os.path.join(current_dir, "../alias")
 
 override_all = False
 
